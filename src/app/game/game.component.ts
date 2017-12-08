1f4c5a3547c6e22bebf13085379004cdbb6e6cc1
kabi_rules <javifont@gmail.com>
2017-12-08 23:55:25 +0100

    game component & assets

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,13 @@
+
+// Import component decorator
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+  
+})
+
+// Component class
+export class GameComponent {}

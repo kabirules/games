2caf50ca2e8c88389cb4890765c4b2d7e9bda20f
Javier Font <javifont@gmail.com>
2017-12-09 14:46:38 +0100

    Service implemented

--- src/app/dog-list/dog-list.component.ts
+++ src/app/dog-list/dog-list.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
+import { DogService } from './dog.service';
 
 @Component({
   selector: 'dog-list',
   templateUrl: './dog-list.component.html',
   styleUrls: ['./dog-list.component.css'],
-   providers: []
+   providers: [DogService]
 })
 
-// Component class
 export class DogListComponent {
-  dogs = [
-  {
-  	name:'Sisqo', 
-  	age:'11 años', 
-    description:'Sisqo es un perro muy amoroso, ya esta viejito y no escucha muy bien. Le encanta cazar ratas.', 
-    picture:'../../assets/images/dogs-pictures/0.jpg'
-  },{
-  	name:'Cerbero', 
-  	age:'1 año', 
-  	description:'Cerbero es un perro con mucha energia, necesita un hogar donde tenga mucho espacio', 
-  	picture:'../../assets/images/dogs-pictures/1.jpg'
+  dogs = [];
+
+  //creamos el constructor, donde incializamos el servicio
+   constructor(private dogService: DogService) { }
+
+   getDogs(): void {
+    //la funcion obtiene los perritos del servicio
+    this.dogs = this.dogService.getDogsList();
   }
- ];
+  //esta función es llamada solo una vez 
+  //la utilizaremos para inicializar los perritos
+  ngOnInit(): void {
+    this.getDogs(); 
+  }
+
 }
---
+++ src/app/dog-list/dog.ts
@@ -0,0 +1,6 @@
+export class Dog {
+    name: string;
+    age: string;
+    description: string;
+    picture: string;
+  }

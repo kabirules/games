22c26a2d2b3c18b944b981e391c4657da173097d
Javier Font <javifont@gmail.com>
2017-12-16 12:56:28 +0100

    Nav do not style correctly, fix it!

--- src/app/game-list/games-json.ts
+++ src/app/game-list/games-json.ts
@@ -37,6 +37,13 @@ export const GAMES: Game[] = [
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
 		url: "https://games.poki.com/458768/colorswitchchallenges"
+	},	
+	{
+		id: "colorswitchchallenges",
+		name: "Color Switch Challenges",
+		description: "The best game ever!",
+		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
+		url: "https://games.poki.com/458768/colorswitchchallenges"
 	}			
 ];
 

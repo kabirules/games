776e664a52de7824383948059a3c19c2f3c4a9f2
kabi_rules <javifont@gmail.com>
2018-01-05 23:53:25 +0100

    Minor changes

--- src/app/game-list/games-json.ts
+++ src/app/game-list/games-json.ts
@@ -8,42 +8,56 @@ export const GAMES: Game[] = [
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
+		url: "http://games.poki.com/458768/colorswitchchallenges"
 	},
 	{
 		id: "colorswitchchallenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
+		url: "http://games.poki.com/458768/colorswitchchallenges"
 	},
 	{
 		id: "colorswitchchallenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
+		url: "http://games.poki.com/458768/colorswitchchallenges"
 	},
 	{
 		id: "colorswitchchallenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
+		url: "http://games.poki.com/458768/colorswitchchallenges"
 	},
 	{
 		id: "colorswitchchallenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
-	},	
+		url: "http://games.poki.com/458768/colorswitchchallenges"
+	},
 	{
 		id: "colorswitchchallenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "https://games.poki.com/458768/colorswitchchallenges"
+		url: "http://games.poki.com/458768/colorswitchchallenges"
+	},
+	{
+		id: "colorswitchchallenges",
+		name: "Color Switch Challenges",
+		description: "The best game ever!",
+		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
+		url: "http://games.poki.com/458768/colorswitchchallenges"
+	},			
+	{
+		id: "wanderersio",
+		name: "Wanderers.io",
+		description: "The best game ever!",
+		image: "http://img.poki.com/f98b18e9-e210-450b-9be4-65ea028a1ae3_1.jpg",
+		url: "http://games.poki.com/458768/colorswitchchallenges"
 	}			
 ];
 

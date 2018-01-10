275b677bb7990e205105467827776879f6a6c371
kabi_rules <javifont@gmail.com>
2018-01-10 20:30:10 +0100

    Two new games

--- src/app/game-list/games-json.ts
+++ src/app/game-list/games-json.ts
@@ -11,18 +11,25 @@ export const GAMES: Game[] = [
 		url: "http://games.gamepix.com/play/40376?sid=90269"
 	},
 	{
-		id: "colorswitchchallenges",
+		id: "color-switch-challenges",
 		name: "Color Switch Challenges",
 		description: "The best game ever!",
 		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
 		url: "http://games.poki.com/458768/colorswitchchallenges"
 	},
 	{
-		id: "colorswitchchallenges",
-		name: "Color Switch Challenges",
+		id: "prism",
+		name: "Prism",
 		description: "The best game ever!",
-		image: "http://img.poki.com/40e2a174-963c-42e6-96cf-53599fc5b8ea_1.jpg",
-		url: "http://games.poki.com/458768/colorswitchchallenges"
+		image: "https://cdn.wtf/g/b339f236-9bda-46cb-ae26-d0d255bae4fd/meta/promo_440.png",
+		url: "https://clay.io/game/prism"
+	},	
+	{
+		id: "little-alchemy",
+		name: "Little Alchemy",
+		description: "The best game ever!",
+		image: "https://cdn.wtf/g/42db4b0a-464e-4ae6-b06e-96440ce66573/meta/promo_440.png",
+		url: "https://clay.io/game/littlealchemy"
 	}			
 ];
 
